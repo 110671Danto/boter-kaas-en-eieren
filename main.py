d52ee16aa34267fccf45b7bb5e391c3919ba443e
Danto Rosier <74553165+110671Danto@users.noreply.github.com>
2022-09-19 14:29:54 +0000

    results functie toegevoegd zodat je kan zie hoe vaak er gewonnen wordt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 from bke import EvaluationAgent, start, can_win, RandomAgent
 
+gamestate = 1
+
 
 class MijnSpeler(EvaluationAgent):
   def evaluate(self, board, my_symbol, opponent_symbol):
@@ -16,4 +18,20 @@ class MyrandomAgent(RandomAgent):
 
 my_random_agent = MyrandomAgent()
 mijn_speler = MijnSpeler()
-start(player_o = mijn_speler, player_x = my_random_agent)
+
+results = {"O":0, "X":0, "None":0}
+if  gamestate == 1: 
+  for i in range(100):
+    result = start(player_o = mijn_speler, player_x = my_random_agent)
+    
+    if result == "X":
+      results["X"] += 1
+    elif result == "O":
+      results["O"] += 1
+    else:
+      results["None"] += 1
+
+print(results)
+  
+def menu(): 
+  print("druk op 1 om het spel te start")
