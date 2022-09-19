bc2bab40748ca2caa5fa29cb5c67a0cd83caed01
Danto Rosier <74553165+110671Danto@users.noreply.github.com>
2022-09-19 13:48:29 +0000

    ik heb gemaakt dat een willekeurige tegenstander tegen een slimme tegenstander van de template wordt gezet.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import random
-from bke import EvaluationAgent, start
+from bke import EvaluationAgent, start, can_win, RandomAgent
 
 
-class MyRandomAgent(EvaluationAgent):
-  def evaluatie(self, board, my_symbol, opponent_symbol):
-    return random.randint(1,500)
+class MijnSpeler(EvaluationAgent):
+  def evaluate(self, board, my_symbol, opponent_symbol):
+    getal = 1
+    if can_win(board, opponent_symbol):
+      getal = getal - 1000
 
+    return getal
 
-  My_random_agent = MyRandomAgent()
-start(player_0 = My_Random_Agent)
+class MyrandomAgent(RandomAgent):
+  def evaluate(self, board, my_symbol, opponent_symbol):
+    return random.randint(1, 500)
+
+my_random_agent = MyrandomAgent()
+mijn_speler = MijnSpeler()
+start(player_o = mijn_speler, player_x = my_random_agent)
